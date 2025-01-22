fe377010d9d7d043bb77aa45fd4a0c9fc9c3e012
Seth Borrowman <seth.borrowman@northwestern.edu>
2025-01-22 12:14:32 -0600

    add contigs parameter

--- rename_contigs.py
+++ rename_contigs.py
@@ -3,16 +3,28 @@ import argparse
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('-i', '--input', help='input FASTA', required=True)
-  parser.add_argument('-b', '--bedfile', help='bedfile with contig', required=True)
+  parser.add_argument('-b', '--bedfile', help='bedfile with contig', required=False)
   parser.add_argument('-o', '--output', help='output FASTA', required=True)
+  parser.add_argument('-c', '--contigs', help='contig names', required=False, nargs='+')
   args = parser.parse_args()
 
-  bedfile = open(args.bedfile)
-  bedlines = bedfile.readlines()
   new_names = []
-  for i in bedlines:
-    new_names.append(i.split('	')[3])
-  bedfile.close()
+
+  if args.bedfile:
+    bedfile = open(args.bedfile)
+    bedlines = bedfile.readlines()
+    for i in bedlines:
+      new_names.append(i.split('	')[3])
+    bedfile.close()
+    print(new_names)
+  elif args.contigs:
+    new_names.append(args.contigs)
+    new_names=new_names[0]
+    new_names=[i + "\n" for i in new_names]
+    print(new_names)
+  else:
+    print("Either a bedfile or array of contig names must be specified")
+    exit()
 
   in_fasta = open(args.input)
   out_fasta = open(args.output, 'w')
